5711a958d66c3cdbecb452301175b74a1a1e1901
Donnie Hartman <hartman_donnie@hotmail.com>
2023-06-10 20:26:10 -0400

    Updated meta tags in webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
         path: path.resolve(__dirname, './view'),
         publicPath: '/',
         filename: `[name]-${timestamp}.js`,
+        assetModuleFilename: '[name][ext]',
         clean: true
     },
     module: {
@@ -69,7 +70,19 @@ module.exports = {
                     `<link rel="manifest" href="/site-${timestamp}.webmanifest" />`,
                     `<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-${timestamp}.png" />`,
                     `<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32-${timestamp}.png" />`,
-                    `<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16-${timestamp}.png" />`
+                    `<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16-${timestamp}.png" />`,
+                    `<meta property="og:url" content="https://kristawaltersflrealtor.com">`,
+                    `<meta property="og:type" content="website">`,
+                    `<meta property="og:title" content="Krista Walters | Florida Realtor">`,
+                    `<meta property="og:description" content="Your trusted partner in Florida real estate">`,
+                    `<meta property="og:image" content="https://kristawaltersflrealtor.com/krista-walters-headshot.jpg">`,
+                    `<meta name="twitter:card" content="summary_large_image">`,
+                    `<meta property="twitter:domain" content="kristawaltersflrealtor.com">`,
+                    `<meta property="twitter:url" content="https://kristawaltersflrealtor.com">`,
+                    `<meta name="twitter:title" content="Krista Walters | Florida Realtor">`,
+                    `<meta name="twitter:description" content="Your trusted partner in Florida real estate">`,
+                    `<meta name="twitter:image" content="https://kristawaltersflrealtor.com/krista-walters-headshot.jpg">`,
+                    `<meta name="description" content="Your trusted partner in Florida real estate">`
                 ]
             },
             inject: true
