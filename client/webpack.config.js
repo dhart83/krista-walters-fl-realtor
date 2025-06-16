6d56a1d3c849e3fd4b9111222b0e505ad07ab367
Donnie Hartman <hartman_donnie@hotmail.com>
2025-06-15 23:17:38 -0400

    🔧 fix(manifest): remove leading slash to fix GitHub Pages 404

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
             templateParameters: {
                 headTags: [
                     `<link rel="icon" href="/favicon-${timestamp}.ico" />`,
-                    `<link rel="manifest" href="/site.webmanifest" />`,
+                    `<link rel="manifest" href="site.webmanifest" />`,
                     `<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-${timestamp}.png" />`,
                     `<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32-${timestamp}.png" />`,
                     `<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16-${timestamp}.png" />`,
